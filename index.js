8a8d2625278b09342b3be6d58afca8280efebd5a
guillermot <guillet84@gmail.com>
2017-08-27 01:03:46 -0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const content = '<loginTicketRequest><header><uniqueId>1</uniqueId><generationTime>2017-08-26T23:50:46</generationTime><expirationTime>2017-08-27T23:10:46</expirationTime></header><service>wsfe</service></loginTicketRequest>';
 const pem = require("pem");
 const fs = require("fs");
 const opensslSigner = require("./openssl-signer");
@@ -8,6 +7,7 @@ pem.config({
     pathOpenSSL: 'C:\\OpenSSL-Win32\\bin\\openssl.exe'
 });
 
+const content = '<loginTicketRequest><header><uniqueId>1</uniqueId><generationTime>2017-08-26T23:50:46</generationTime><expirationTime>2017-08-27T23:10:46</expirationTime></header><service>wsfe</service></loginTicketRequest>';
 const config = {
     content: content,
     fileContentPath: 'C:\\projects\\afip-login-cms\\LoginTicketRequest.xml',
